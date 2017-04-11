be5f6830a3c2c782b2d8498cfbea01c57b9c20d4
gabystern <stern.gabrielle@gmail.com>
2017-04-11 17:43:54 -0400

    in progress

---
+++ lib/schema.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE IF NOT EXISTS series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+);
